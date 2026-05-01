bc42c92a45df8400829f3f5dc1ba30ae1f209073
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-01 18:23:38 +0100

    first javascript class

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Hello world');
+
+let name = "Olukayode";
+let fullName = "Olukayode Asemudara";
+console.log(fullName);
+
+console.log(`Hello there, my name is ${fullName} and I am a student at Semicolon Africa`);
